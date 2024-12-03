e1cec0cc319cce23c05b77a0f57146deb78e0e94
Patrick Strang <patrick.strang@mirado.com>
2024-12-03 22:13:17 +0100

    part 2 day 3

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -1,20 +1,30 @@
+import re
+
+
 def read_input(file_path: str) -> str:
     with open(file_path, "r") as file:
         return file.read()
 
 
 def parse_input(input_data: str) -> int:
-    import re
-
     pattern = r"mul\((?P<a>\d+),(?P<b>\d+)\)"
     matches = re.finditer(pattern, input_data)
     return sum(int(match.group("a")) * int(match.group("b")) for match in matches)
+
+
+def clean_parse(input_data: str) -> int:
+    pattern = r"don't\(\).*?(?=do\(\)|$)"
+    cleaned_text = re.sub(pattern, "", input_data.replace("\n", ""))
+    print(cleaned_text)
+
+    return parse_input(cleaned_text)
 
 
 def main():
     print("Day 03 - 2024")
     input_data = read_input("input.txt")
     print(parse_input(input_data))
+    print(clean_parse(input_data))
 
 
 if __name__ == "__main__":
--- 2024/03/test_main.py
+++ 2024/03/test_main.py
@@ -1,6 +1,11 @@
-from main import parse_input, read_input
+from main import parse_input, clean_parse, read_input
 
 
 def test_parse_input():
     input_data = read_input("test_input.txt")
     assert parse_input(input_data) == 161
+
+
+def test_parse_input2():
+    input_data = read_input("test_input2.txt")
+    assert clean_parse(input_data) == 48
