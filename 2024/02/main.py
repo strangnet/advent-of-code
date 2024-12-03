3ec129b32a9075292a71bd36a1edf6a1ac038841
Patrick Strang <patrick.strang@mirado.com>
2024-12-03 21:18:49 +0100

    part2 day 2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+
 def read_input(file_path: str) -> list[list[int]]:
     with open(file_path, "r") as file:
         return [list(map(int, line.split())) for line in file]
@@ -6,25 +9,49 @@ def read_input(file_path: str) -> list[list[int]]:
 def calculate_safe_reports(lists: list[list[int]]) -> int:
     counter = 0
     for list in lists:
-        safe = True
-        deviation = 0
-        for i in range(1, len(list)):
-            diff = list[i] - list[i - 1]
-            if diff == 0:
-                safe = False
-                break
+        safe = is_safe(list)
+        counter += 1 if safe else 0
+    return counter
 
-            if abs(diff) not in range(1, 4):
-                safe = False
-                break
 
-            dev = abs(diff) / diff
-            if deviation == 0:
-                deviation = dev
-            elif deviation != dev:
-                safe = False
-                break
+def is_safe(list: list[int]) -> bool:
+    safe = True
+    deviation = 0
+    for i in range(1, len(list)):
+        diff = list[i] - list[i - 1]
+        if diff == 0:
+            safe = False
+            break
 
+        if abs(diff) not in range(1, 4):
+            safe = False
+            break
+
+        dev = abs(diff) / diff
+        if deviation == 0:
+            deviation = dev
+        elif deviation != dev:
+            safe = False
+            break
+
+    return safe
+
+
+def is_safe_with_dampener(list: list[int]) -> bool:
+    if is_safe(list):
+        return True
+    for i in range(len(list)):
+        if is_safe(list[:i] + list[i + 1 :]):
+            print(i)
+            return True
+
+    return False
+
+
+def calculate_safe_reports_with_dampener(lists: list[list[int]]) -> int:
+    counter = 0
+    for list in lists:
+        safe = is_safe_with_dampener(list)
         counter += 1 if safe else 0
     return counter
 
@@ -33,6 +60,7 @@ def main():
     print("Day 02 - 2024")
     input_data = read_input("input.txt")
     print(calculate_safe_reports(input_data))
+    print(calculate_safe_reports_with_dampener(input_data))
 
 
 if __name__ == "__main__":
--- 2024/02/test_main.py
+++ 2024/02/test_main.py
@@ -1,6 +1,15 @@
-from main import calculate_safe_reports, read_input
+from main import (
+    calculate_safe_reports,
+    calculate_safe_reports_with_dampener,
+    read_input,
+)
 
 
 def test_calculate_safe_reports():
     lists = read_input("test_input.txt")
     assert calculate_safe_reports(lists) == 2
+
+
+def test_calculate_safe_reports_with_dampener():
+    lists = read_input("test_input.txt")
+    assert calculate_safe_reports_with_dampener(lists) == 4
