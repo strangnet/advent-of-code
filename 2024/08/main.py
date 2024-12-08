55be179e35cedba65b537acd9ead0ff3ac82b79f
Patrick S <patrick@strang.io>
2024-12-08 10:43:31 +0100

    part 2 day 8

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -24,14 +24,53 @@ def calculate_distance(antenna1, antenna2) -> tuple[int, int]:
 def find_anti_nodes(antennas: dict[str, list[tuple[int, int]]]):
     an = set()
     for antenna, nodes in antennas.items():
-        permutations = list(itertools.combinations(nodes, 2))
-        for node_pair in permutations:
+        combinations = list(itertools.combinations(nodes, 2))
+        for node_pair in combinations:
             distance = calculate_distance(node_pair[0], node_pair[1])
             anti_nodes = (
                 add_tuples(node_pair[0], distance),
                 add_tuples(node_pair[1], (-distance[0], -distance[1])),
             )
             for anti_node in anti_nodes:
+                an.add(anti_node)
+    return an
+
+
+def find_anti_nodes_with_harmonics(
+    antennas: dict[str, list[tuple[int, int]]], width: int, height: int
+):
+    an = set()
+    for antenna, nodes in antennas.items():
+        combinations = list(itertools.combinations(nodes, 2))
+        for node_pair in combinations:
+            distance = calculate_distance(node_pair[0], node_pair[1])
+            round = -1  # now we're talking! :)
+            while True:
+                round += 1
+                anti_node = add_tuples(
+                    node_pair[0], (distance[0] * round, distance[1] * round)
+                )
+                if (
+                    anti_node[0] >= height
+                    or anti_node[1] >= width
+                    or anti_node[0] < 0
+                    or anti_node[1] < 0
+                ):
+                    break
+                an.add(anti_node)
+            round = -1
+            while True:
+                round += 1
+                anti_node = add_tuples(
+                    node_pair[1], (-distance[0] * round, -distance[1] * round)
+                )
+                if (
+                    anti_node[0] >= height
+                    or anti_node[1] >= width
+                    or anti_node[0] < 0
+                    or anti_node[1] < 0
+                ):
+                    break
                 an.add(anti_node)
     return an
 
@@ -51,6 +90,8 @@ def main():
         if (node[0] < height and node[1] < width) and (node[0] >= 0 and node[1] >= 0):
             count += 1
     print(f"count: {count}")
+    anh = find_anti_nodes_with_harmonics(antennas, width, height)
+    print(f"count with harmonics: {len(anh)}")
 
 
 if __name__ == "__main__":
